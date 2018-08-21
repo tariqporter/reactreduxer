46c5ac46ec4caaf5202822c691e7c9c8b043361d
Tariq Porter <tic084@gmail.com>
2018-08-21 13:09:19 -0400

    Add App unit test file

--- templates/new/src/components/App.test.tsx
+++ templates/new/src/components/App.test.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { mount, ReactWrapper } from "enzyme";
 import { App } from './App';
+import { store } from '../index';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let wrapper: ReactWrapper;
+  const store = createStore(rootReducer);
+  beforeEach(() => {
+    wrapper = mount(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(wrapper.getElement(), div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
