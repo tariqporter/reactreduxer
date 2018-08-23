9bcd29899b3c5bbdb97de6525107ba120b5b5231
Tariq Porter <tic084@gmail.com>
2018-08-23 00:11:21 -0400

    Use thunk in App.test.tsx

--- templates/new/src/components/App.test.tsx
+++ templates/new/src/components/App.test.tsx
@@ -2,14 +2,18 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { mount, ReactWrapper } from "enzyme";
 import { App } from './App';
-import { store } from '../index';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 describe('App', () => {
   let wrapper: ReactWrapper;
-  const store = createStore(rootReducer);
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+  );
+
   beforeEach(() => {
     wrapper = mount(
       <Provider store={store}>
